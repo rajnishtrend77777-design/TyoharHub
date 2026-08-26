164afeb2352a0fe0373bf29f09d7b994b2e21b23
rajnishtrend77777-design <rajnishtrend77777@gmail.com>
2026-08-26 23:27:08 +0530

    Fix formatting issues in server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get("/sitemap.xml",(req,res)=>{
   const urls=[
     "/"
   ];
-
+  
   const xml=`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.map(url=>`
